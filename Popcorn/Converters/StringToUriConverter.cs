b272985f6718808e8ce1c50a795e6ca4f559eca5
Benjamin Bougot <bbougot@gmail.com>
2017-02-13 10:18:15 +0100

    Release 1.5.0
    Add update system based on Github
    Improve async image loading

---
+++ Popcorn/Converters/StringToUriConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace Popcorn.Converters
+{
+    /// <summary>
+    /// Used to check if the path to the image file is empty or not
+    /// </summary>
+    public class StringToUriConverter : IValueConverter
+    {
+        /// <summary>
+        /// Convert a path image to a bitmap-cached image
+        /// </summary>
+        /// <param name="value">The value produced by the binding source.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>Cached image</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (string.IsNullOrEmpty(value?.ToString())) return null;
+            var path = value.ToString();
+            return new Uri(path, UriKind.Absolute);
+        }
+
+        /// <summary>
+        /// Not supported
+        /// </summary>
+        /// <param name="value">The value produced by the binding source.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        public object ConvertBack(object value, Type targetType, object parameter,
+            CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
